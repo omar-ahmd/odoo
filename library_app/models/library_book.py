5ff740bb0a52c815d9c9fe50f156b974e73c6ba7
omar-ahmd <omaharmad@gmail.com>
2020-10-03 22:10:25 +0300

    Add check ISBN functions to the check button

--- library_app/models/library_book.py
+++ library_app/models/library_book.py
@@ -1,7 +1,20 @@
-from odoo import fields, models
+from odoo import api, fields, models
+
+
 class Book(models.Model):
     _name = 'library.book'
     _description = 'Book'
+    @api.model_create_multi
+    def _check_isbn(self):
+        self.ensure_one()
+        digits = [int(x) for x in self.isbn if x.isdigit()]
+        if len(digits) == 13:
+            ponderations = [1, 3] * 6
+            terms = [a * b for a, b in zip(digits[:12], ponderations)]
+            remain = sum(terms) % 10
+            check = 10 - remain if remain != 0 else 0
+            return digits[-1] == check
+
     name = fields.Char('Title', required=True)
     isbn = fields.Char('ISBN')
     active = fields.Boolean('Active?', default=True)
